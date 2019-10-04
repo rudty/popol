2f00058b10002e57bc09d2174973e53c009dbdc9
rudty <rudtyz@gmail.com>
2019-10-04 11:39:04 +0900

    Updates

---
+++ precache-manifest.5eddc031e615f625aeb77ed7b5433e93.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6834708faae69f50c57af4b5ea4ce582",
+    "url": "/popol/index.html"
+  },
+  {
+    "revision": "82505d94cdfbc6d3049e",
+    "url": "/popol/static/css/main.57d67e11.chunk.css"
+  },
+  {
+    "revision": "49b70c56fb78246485c6",
+    "url": "/popol/static/js/2.ea55a14a.chunk.js"
+  },
+  {
+    "revision": "82505d94cdfbc6d3049e",
+    "url": "/popol/static/js/main.43649c30.chunk.js"
+  },
+  {
+    "revision": "6f4506cdc28044ce7c25",
+    "url": "/popol/static/js/runtime~main.d6cd0002.js"
+  },
+  {
+    "revision": "23c0f56f47fda7ac9ecf2ffcd2bf36d9",
+    "url": "/popol/static/media/MuteSpeakerOnHeadsetUnplugged.23c0f56f.png"
+  },
+  {
+    "revision": "b23ba4fddc47f412e0660bb0d911ac8b",
+    "url": "/popol/static/media/backgroundcode.b23ba4fd.png"
+  },
+  {
+    "revision": "ffeea087c0193680eb65b06e178335c1",
+    "url": "/popol/static/media/cpp.ffeea087.jpg"
+  },
+  {
+    "revision": "fc838011c88ac0b625b0c7c61dc7bdb6",
+    "url": "/popol/static/media/java.fc838011.jpg"
+  },
+  {
+    "revision": "11e8af26618daecab1b13cab7387ce65",
+    "url": "/popol/static/media/javascript.11e8af26.png"
+  },
+  {
+    "revision": "1d7de53bfaca62e2567e977479b74e17",
+    "url": "/popol/static/media/kotlin.1d7de53b.jpg"
+  },
+  {
+    "revision": "adc989a017c43040ae2bdb0342922467",
+    "url": "/popol/static/media/mysql.adc989a0.jpg"
+  },
+  {
+    "revision": "186db54728d271fba9e5e2fc6f78beb8",
+    "url": "/popol/static/media/nodekell.186db547.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/popol/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/popol/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/popol/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/popol/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "c970a280bf19a3bb5b77f25ab8afd31e",
+    "url": "/popol/static/media/sqlserver.c970a280.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c999439dc162b88786143815da6b987f.js"
+  "/popol/precache-manifest.5eddc031e615f625aeb77ed7b5433e93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/popol/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
