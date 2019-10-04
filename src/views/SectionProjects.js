0db8189e3e868630d22a402c9fdf2b64515f30e2
rudty <rudtyz@gmail.com>
2019-10-04 21:45:33 +0900

    성과

--- src/views/SectionProjects.js
+++ src/views/SectionProjects.js
@@ -12,7 +12,46 @@ function SectionProjects() {
         <div className="ml-auto mr-auto text-center" md="8">
           <h2 className="title">Projects</h2>
           <p className="description">
-            개인 프로젝트
+            <h3>주요 성과</h3>
+            <Container className="text-center">
+              <Row>
+                <Col className="text-left" md="8">
+                  <div className="typography-line">
+                    <span>2019.08 - 2019.9</span>
+                    <p className="blockquote blockquote-primary">
+                      <b>인게임 콘텐츠 저장 기능 구현</b><br />
+                      <br />
+                      인게임에서 유저가 플레이 한 내역을 저장 및 관리할수 있게 하는 기능을 구현 <br />
+                      저장 데이터는 유저가 직접 선택 가능<br />
+                      C++ 게임 서버 및 node js 웹 서버 사용<br />
+                    </p>
+                  </div>
+                  <div className="typography-line">
+                    <span>2019.03. - 2019.06</span>
+                    <p className="blockquote blockquote-primary">
+                      <b>아웃 게임 유저 콘텐츠 로비 개편</b><br />
+                      <br />
+                      아웃 게임에서 유저들이 만든 맵들의 정렬방식 및 기능 개편<br />
+                      검색 관련 서버 SQL 쿼리 리뉴얼 및 back-end 리뉴얼 <br />
+                      인게임과 연관된 부분은 C++ 게임 서버, 관련없는 부분은 node js 웹 서버 사용<br />
+                    </p>
+                  </div>
+                  <div className="typography-line">
+                    <span>2018.10 - 2018.12</span>
+                    <p className="blockquote blockquote-primary">
+                      <b>아웃 게임 웹 콘텐츠 개발</b><br />
+                      <br />
+                      인게임에서 유저들이 만든 콘텐츠들을 웹에서 볼 수 있게하는 back-end 서버 개발<br />
+                      선택 시 해당 콘텐츠에 즉시 입장가능하게 구현<br />
+                      인게임과 연관된 부분은 C++ 게임 서버, 관련없는 부분은 node js 웹 서버 사용<br />
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </p>
+          <p className="description">
+            <h3>개인 프로젝트</h3>
           </p>
         </div>
         <div className="space-50"></div>
@@ -111,7 +150,7 @@ function SectionProjects() {
               </p>
             </Col>
           </Row>
-          
+
         </Container>
       </div>
     </>
